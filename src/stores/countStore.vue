4e34323ce4998854a897f00bfb60ffdea67c3d0d
ChiakiIzutsu <chiaki9960@icloud.com>
2023-12-19 21:35:11 +0900

    Pinia学習

---
+++ src/stores/countStore.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <p>count:{{ counterStore.state.count }}</p>
+    <button @click="counterStore.increment">increment</button>
+  </div>
+</template>
+<script setup lang="ts">
+import { useCounterStore } from '../stores/counter'
+
+const counterStore = useCounterStore()
+</script>
+
+<!-- 
+3行目
+  counterStoreのstateのcount、つまり初期値がマスタッシュに出力される
+4行目
+  クリックイベントが発生した際、counterStoreのincrement処理が発生するよう記述
+
+8行目
+  定義したuseCounterStoreをインポート、呼び出している
+
+10行目
+  conterStoreはuseCounterStoreと同じと宣言している
+-->
+
+<!-- クリックするたびにcountはインクリメントするため1プラス、ダブルカウントはインクリメントされるたびにcomputedにて2倍の計算をする -->
--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -3,19 +3,28 @@ import { defineStore } from 'pinia'
 
 interface CounterState {
   count: number
-} //interfaceで型注釈
+}
 export const useCounterStore = defineStore('counter', () => {
   const state = ref<CounterState>({
-    count: 3
-  }) //optionsAPIだとstateプロパティにあたる、初期値を設定
-  const doubleCount = computed(() => state.value.count * 2) //optionsAPIだとgettersにあたる。データの加工、算出をする
+    count: 0
+  })
+  const doubleCount = computed(() => state.value.count * 2)
+
   function increment() {
-    //optionsAPIだとactionsにあたる、methodsと同じ役割
     state.value.count++
   }
 
   return { state, doubleCount, increment }
 })
+
+// 4行目
+//interfaceで型注釈
+// 7行目
+//optionsAPIだとstateプロパティにあたる、初期値を設定
+// 11行目
+//optionsAPIだとgettersにあたる。データの加工、算出をする
+// 13行目
+//optionsAPIだとactionsにあたる、methodsと同じ役割
 
 //https://pinia.vuejs.org/core-concepts/ 公式ドキュメント
 //use + ストア名 + Storeとキャメル形式で記述するのがお作法、決まり。
