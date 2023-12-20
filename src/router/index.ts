709468bc4ca3ac37d4118ed70acab09b242b1e14
ChiakiIzutsu <chiaki9960@icloud.com>
2023-12-20 19:56:51 +0900

    v-bind

--- src/router/index.ts
+++ src/router/index.ts
@@ -40,6 +40,14 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/reactiveObject.vue')
+    },
+    {
+      path: '/vind',
+      name: 'vBind',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/vBind.vue')
     }
   ]
 })
---
+++ src/views/vBind.vue
@@ -0,0 +1,10 @@
+<template>
+  <p><img src="../assets//logo.svg" alt="VueLogo" :[widthOrHeight]="widthOrHeightValue" /></p>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const widthOrHeight = ref('height')
+const widthOrHeightValue = ref(100)
+</script>
